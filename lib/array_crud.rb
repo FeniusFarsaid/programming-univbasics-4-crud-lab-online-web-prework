f3ba034306ededcc057c90ba379feb01d89a32b2
David Ingram <david@ingramaviation.com>
2019-07-17 18:06:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["Mexico", "Guatemala", "Belize", "El_Salvador"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(create_an_array, "Honduras")
   create_an_array << "Honduras"
 end
 
